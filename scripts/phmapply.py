4f8b05bd59673f40257e3ddbf815cd88396d07a9
Bruno Quint <bquint@ctio.noao.edu>
2016-11-02 02:00:12 -0300

    Code cleanup on phmapply.py

--- scripts/phmapply.py
+++ scripts/phmapply.py
@@ -20,10 +20,17 @@ from scipy.interpolate import UnivariateSpline
 
 
 def main():
-
     # Setting Options ---------------------------------------------------------
     parser = argparse.ArgumentParser(
         description="Apply a phase-map on a data-cube."
+    )
+    parser.add_argument(
+        '-c', '--center', action='store_true',
+        help="Try to center the strongest line in the cube."
+    )
+    parser.add_argument(
+        '-n', '--npoints', type=int, default=10,
+        help="Number of points in the re-sampling for channel [10]."
     )
     parser.add_argument(
         '-o', '--output', metavar='output', type=str, default=None,
@@ -32,10 +39,6 @@ def main():
     parser.add_argument(
         '-q', '--quiet', action='store_true',
         help="Run it quietly."
-    )
-    parser.add_argument(
-        '-n', '--npoints', type=int, default=10,
-        help="Number of points in the re-sampling for channel [10]."
     )
     parser.add_argument(
         'cube_file', metavar='cube_file', type=str,
@@ -48,7 +51,6 @@ def main():
 
     args = parser.parse_args()
     v = not args.quiet
-    loading = [' ','-','\\','|','/']
 
     # Printing program header --------------------------------------------------
     if v:
@@ -103,8 +105,8 @@ def main():
         print("[!] Leaving now.\n")
         sys.exit()
 
-    M = data_cube.header['NAXIS1']
-    N = data_cube.header['NAXIS2']
+    m = data_cube.header['NAXIS1']
+    n = data_cube.header['NAXIS2']
 
     ref_x = phase_map.header['PHMREFX']
     ref_y = phase_map.header['PHMREFY']
@@ -119,19 +121,19 @@ def main():
         # TODO the phase-map or while fitting it.
         # TODO or even to give the option for the user to enter it.
         # FSR = phase_map.header['PHMFITSR']
-        FSR = phase_map.header['PHMFSR']
+        f_s_r = phase_map.header['PHMFSR']
         if v:
-            print(" Free Spectral Range = %.2f %s" % (FSR, units))
+            print(" Free Spectral Range = %.2f %s" % (f_s_r, units))
 
-    except (KeyError):
+    except KeyError:
         print(" Please, enter the free-spectral-range in %s units" % units)
-        FSR = input(" > ")
+        f_s_r = input(" > ")
 
-    FSR = round(FSR / abs(sample)) # From BCV to Channels
+    f_s_r = round(f_s_r / abs(sample))  # From BCV to Channels
     if v:
-        print(" Free-Spectral-Range is %d channels" % FSR)
+        print(" Free-Spectral-Range is %d channels" % f_s_r)
 
-    fsr = FSR * args.npoints # From Channels to nPoints
+    fsr = f_s_r * args.npoints  # From Channels to nPoints
     fsr = int(round(fsr))
     if v:
         print(" Free-Spectral-Range is %d points" % fsr)
@@ -159,21 +161,21 @@ def main():
     z = np.arange(3 * n_channels) - n_channels
     new_z = np.arange(0, z.size, 1.0 / args.npoints)
 
-    for i in range(M):
-        for j in range(N):
+    for i in range(m):
+        for j in range(n):
 
             # Extracting a spectrum
-            spec = data_cube.data[:,j,i]
+            spec = data_cube.data[:, j, i]
             spec = np.hstack((spec, spec, spec))
-            dz = phase_map.data[j,i]
+            dz = phase_map.data[j, i]
 
             # Re-sample spectrum
             spline = UnivariateSpline(z, spec, s=0.0)
             new_spec = spline(new_z)
 
             # Cutting spectrum
-            new_z = new_z[0:fsr+1]
-            new_spec = new_spec[0:fsr+1]
+            new_z = new_z[0:fsr + 1]
+            new_spec = new_spec[0:fsr + 1]
 
             # Shifting spectrum
             new_spec = np.roll(new_spec, int(dz))
@@ -183,123 +185,92 @@ def main():
             spec = spline(z)
 
             # Storing new spectrum
-            data_cube.data[:,j,i] = spec[n_channels:2*n_channels]
+            data_cube.data[:, j, i] = spec[n_channels:2 * n_channels]
 
             # Giving a feedback to the user
             if v:
-                temp = ((i + 1) * 100.00 / M)
+                temp = ((i + 1) * 100.00 / m)
                 sys.stdout.write('\r  %2.2f%% ' % temp)
                 sys.stdout.flush()
 
-    end_of_cube = min(int(round(FSR)), data_cube.data.shape[0])
+    end_of_cube = min(int(round(f_s_r)), data_cube.data.shape[0])
     data_cube.data = data_cube.data[0:end_of_cube, :, :]
-    if v: print(" Done.")
+    if v:
+        print(" Done.")
 
     # Saving corrected data-cube ----------------------------------------------
     if v:
-        print("\n Writing output to file %s." % out_file);
+        print("\n Writing output to file %s." % out_file)
     data_cube.writeto(out_file, clobber=True)
     if v:
-        print(" Done.");
+        print(" Done.")
+        # noinspection PyUnboundLocalVariable
         end = time.time() - start
-        print("\n Total time ellapsed: %02d:%02d:%02d" % \
-              (end // 3600, end % 3600 // 60, end % 60));
-        print(" All done!\n");
+        print("\n Total time ellapsed: {0:02d}:{1:02d}:{2:02d}".format(
+            end // 3600, end % 3600 // 60, end % 60))
+        print(" All done!\n")
 
-def check_instrument(filename, instrument='btfi', keyword='INSTRUME'):
-    """
-    Method written to check the instrument.
-    """
-    header = pyfits.getheader(filename)
 
-    # First check if the keyword exists
-    if not keyword in header:
-        print("")
-        print(" Instrument type not recognized.")
-        print(" Do you want to proceed? [Y or n]")
+# Method shiftSpectrum ========================================================
+# def shiftSpectrum(spec, dz, n_points=100):
+#     dzSign = -np.sign(dz)
+#     dz = abs(dz)
+#     dz_points = int(dz * n_points)
+#     if dz_points is 0: return spec
+#
+#     # Get the spectrum from cube
+#     z = np.arange(spec.size)
+#     spline = UnivariateSpline(z, spec, s=0.0)
+#
+#     # Re-sample spectrum
+#     newZ = np.linspace(z[0], z[-1], z.size * n_points)
+#     newSpec = spline(newZ)
+#
+#     # Add padded borders
+#     newSpec = np.append(np.zeros(dz_points), newSpec)
+#     newSpec = np.append(newSpec, np.zeros(dz_points))
+#
+#     # Shifting spectrum
+#     newSpec = np.roll(newSpec, int(dzSign * dz_points))
+#
+#     # Cutting Spectrum
+#     newSpec = newSpec[dz_points:-dz_points]
+#
+#     # Down-sampling
+#     spline = UnivariateSpline(newZ, newSpec, s=0.0)
+#     spec = spline(z)
+#
+#     return spec
 
-        answer = '.'
-        while answer.lower() not in ' yn':
-            answer = raw_input('? ')
 
-        if answer.lower() == 'n':
-            print(" Leaving now.\n")
-            sys.exit()
-        else:
-            return
-
-def check_mode(filename, keyword='INSTRMOD'):
-    """
-    Return if BTFI was obtained with a Fabry-Perot or with the iBTF.
-    """
-    from astropy.io import fits as pyfits
-
-    header = pyfits.getheader(filename)
-
-    if keyword not in header:
-        warning("Instrument mode not found.")
-        instrument_mode = ''
-        while instrument_mode.lower() not in ['ibtf', 'fp']:
-            instrument_mode = raw_input(" Enter 'ibtf' or 'fp': ")
-    else:
-        if header[keyword].upper() in ['IBTF']:
-            instrument_mode = 'ibtf'
-
-        if header[keyword].upper() in ['FP', 'FABRY-PEROT']:
-            instrument_mode = 'fp'
-
-    return instrument_mode
-
-## Method shiftSpectrum ========================================================
-def shiftSpectrum(spec, dz, nPoints=100):
-
-    dzSign = -np.sign(dz)
-    dz = abs(dz)
-    dzPoints = int(dz * nPoints)
-    if dzPoints is 0: return spec
-
-    # Get the spectrum from cube
-    z = np.arange(spec.size)
-    spline = UnivariateSpline(z, spec, s=0.0)
-
-    # Re-sample spectrum
-    newZ    = np.linspace(z[0], z[-1], z.size * nPoints)
-    newSpec = spline(newZ)
-
-    # Add padded borders
-    newSpec = np.append(np.zeros(dzPoints), newSpec)
-    newSpec = np.append(newSpec, np.zeros(dzPoints))
-
-    # Shifting spectrum
-    newSpec = np.roll(newSpec, int(dzSign * dzPoints))
-
-    # Cutting Spectrum
-    newSpec = newSpec[dzPoints:-dzPoints]
-
-    # Down-sampling
-    spline = UnivariateSpline(newZ, newSpec, s=0.0)
-    spec   = spline(z)
-
-    return spec
-
-## Method shift_spectrum ========================================================
+# Method shift_spectrum ========================================================
 def shift_spectrum(spec, dz, fsr=-1, sample=1.0, n_points=100):
     """
-    @param spec: 1D np.array representing a spectrum to be shifted.
-    @param dz: how big is the shifting.
-    @keyword fsr: a float representing the free-spectra-range in sample units.
-    @keyword sample: a float representing the increment between each channel.
-    @keyword n_points: number of points that will be used for super-sampling.
+    Parameters
+    ----------
+        spec: np.array
+            a spectrum to be shifted.
+
+        dz: float
+            How big is the shifting.
+
+        fsr: float
+            The free-spectra-range in sample units.
+
+        sample: float
+            The increment between each channel.
+
+        n_points: int
+            Number of points that will be used for super-sampling.
     """
+    dz_sign = -np.sign(dz)
+    dz = abs(dz) / sample  # From cube units to channels
+    dz_points = int(dz * n_points)  # From channels to new sample units
 
-    dzSign = -np.sign(dz)
-    dz = abs(dz) / sample # From cube units to channels
-    dzPoints = int(dz * n_points) # From channels to new sample units
+    index = fsr / sample  # From cube units to channels
+    index *= n_points  # From channels to new sample units
 
-    index = fsr / sample # From cube units to channels
-    index = index * n_points # From channels to new sample units
-
-    if dzPoints is 0:
+    if dz_points is 0:
         return spec
 
     # Get the spectrum from cube
@@ -307,35 +278,38 @@ def shift_spectrum(spec, dz, fsr=-1, sample=1.0, n_points=100):
     spline = UnivariateSpline(z, spec, s=0.0)
 
     # Re-sample spectrum
-    newZ = np.linspace(z[0], z[-1], z.size * n_points)
-    newSpec = spline(newZ)
+    new_z = np.linspace(z[0], z[-1], z.size * n_points)
+    new_spec = spline(new_z)
 
     # Cutting Spectrum
-    newSpec = newSpec[0:fsr]
+    new_spec = new_spec[0:fsr]
 
     # Shifting spectrum
-    newSpec = np.roll(newSpec, int(dzSign * dzPoints))
+    new_spec = np.roll(new_spec, int(dz_sign * dz_points))
 
     # Down-sampling
-    spline = UnivariateSpline(newZ, newSpec, s=0.0)
-    spec   = spline(z)
+    spline = UnivariateSpline(new_z, new_spec, s=0.0)
+    spec = spline(z)
 
     return spec
 
+
 def error(my_string):
-    s = bcolors.FAIL + '[ERROR] ' + bcolors.ENDC
+    s = BColors.FAIL + '[ERROR] ' + BColors.ENDC
     s = s + my_string
     print(s)
     return
+
 
 def warning(my_string):
-    s = bcolors.WARNING + '[WARNING] ' + bcolors.ENDC
+    s = BColors.WARNING + '[WARNING] ' + BColors.ENDC
     s = s + my_string
     print(s)
     return
 
-class bcolors:
 
+# noinspection PyClassHasNoInit
+class BColors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
@@ -351,6 +325,7 @@ class bcolors:
         self.FAIL = ''
         self.ENDC = ''
 
-#===============================================================================
+
+# ===============================================================================
 if __name__ == '__main__':
     main()
