18dd5c42decba0d492e8f07664b4f2d4a976abd4
Bruno Quint <bquint@ctio.noao.edu>
2016-12-01 00:40:24 -0300

    Cleated globals.py to store global variables.

---
+++ samfp/globals.py
@@ -0,0 +1,16 @@
+# -*- coding: utf8 -*-
+csv_columns = [
+    'DATE-OBS',
+    'TIME-OBS',
+    'FILENAME',
+    'OBSTYPE',
+    'OBJECT',
+    'RA',
+    'DEC',
+    'AIRMASS',
+    'DECPANGL',
+    'RAPANGL',
+    'NEXTEND',
+    'FILTERS',
+    'FILTER1',
+]
