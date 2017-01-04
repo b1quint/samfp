9b08706582cde706b24e2d28acbbec779a6b2b76
Bruno Quint <bquint@ctio.noao.edu>
2017-01-04 15:31:49 -0300

    changed permissions

--- samfp/dev/wcs_missing.py
+++ samfp/dev/wcs_missing.py
@@ -15,8 +15,8 @@ from astropy.io import fits
 from astropy.wcs import WCS
 from glob import glob
 
-path = '/home/bquint/Data/2016-12-02/RED/'
-file_pattern = '*SO2016B-015*.fits'
+path = '/home/bquint/Data/2016-11-29/RED/'
+file_pattern = '*SO2016B-015*.fits.bz2'
 
 list_of_files = glob(os.path.join(path, file_pattern))
 list_of_files = [os.path.split(f)[-1] for f in list_of_files]
@@ -25,10 +25,11 @@ list_of_files.sort()
 list_with_asol = [f[1:] for f in list_of_files if f[0] == 'a']
 list_wout_asol = [f for f in list_of_files if not f[0] == 'a']
 
-data_frame = pandas.DataFrame(columns=['Column_wout', 'Column_with', 'Object', 'RA', 'DEC', 'Center_Coods'])
+data_frame = pandas.DataFrame(
+    columns=['Column_wout', 'Column_with', 'Object', 'Filter', 'RA', 'DEC',
+             'Center_Coods'])
 
 for f in list_wout_asol:
-
     # Read and fix header before using it ---
     h = fits.getheader(os.path.join(path, f))
     h['equinox'] = 2000.
@@ -47,6 +48,7 @@ for f in list_wout_asol:
     d['Column_wout'] = f
     d['Column_with'] = f if f in list_with_asol else None
     d['Object'] = h['object']
+    d['FILTER'] = h['FILTER1']
     d['RA'] = h['RA']
     d['DEC'] = h['DEC']
     d['Center_Coods'] = c_center.to_string('hmsdms', sep=':', precision=3)
@@ -55,8 +57,10 @@ for f in list_wout_asol:
     s = pandas.Series(data=d)
     data_frame = data_frame.append(s, ignore_index=True)
 
+print("Number of files: {:d} \n"
+      "Number of success: {}".format(len(list_wout_asol),
+                                                     len(
+                                                         list_with_asol)))
+
 df = data_frame[data_frame.isnull().any(axis=1)]
-df.to_csv(os.path.join(path, 'failed_astrometry.csv'))
-
-
-
+df.to_csv(os.path.join(path, 'failed_astrometry2.csv'))
