27098c14a01d18174e12e01cda46c18c3f7b2b34
Bruno Quint <bquint@ctio.noao.edu>
2017-07-17 12:18:17 -0400

    Close #18 - Fixed bug
    It seems that phmfit is back to life

--- samfp/tools/io.py
+++ samfp/tools/io.py
@@ -61,39 +61,39 @@ def safe_save(name, extension=None, overwrite=False, verbose=False):
     return name
 
 
-class MyLogFormatter(logging.Formatter):
-
-    err_fmt = "E %(msg)s"
-    dbg_fmt = "D %(module)s: %(lineno)d: %(msg)s"
-    info_fmt = "I %(msg)s"
-    warn_fmt = "W %(msg)s"
-
-    def __init__(self, fmt="%(levelno)s: %(msg)s"):
-        logging.Formatter.__init__(self, fmt)
-
-    def format(self, record):
-
-        # Save the original format configured by the user
-        # when the logger formatter was instantiated
-        format_orig = self._fmt
-
-        # Replace the original format with one customized by logging level
-        if record.levelno == logging.DEBUG:
-            self._fmt = MyLogFormatter.dbg_fmt
-
-        elif record.levelno == logging.INFO:
-            self._fmt = MyLogFormatter.info_fmt
-
-        elif record.levelno == logging.ERROR:
-            self._fmt = MyLogFormatter.err_fmt
-
-        elif record.levelno == logging.WARNING:
-            self._fmt = MyLogFormatter.warn_fmt
-
-        # Call the original formatter class to do the grunt work
-        result = logging.Formatter.format(self, record)
-
-        # Restore the original format configured by the user
-        self._fmt = format_orig
-
-        return result
+# class MyLogFormatter(logging.Formatter):
+#
+#     err_fmt = "E %(msg)s"
+#     dbg_fmt = "D %(module)s: %(lineno)d: %(msg)s"
+#     info_fmt = "I %(msg)s"
+#     warn_fmt = "W %(msg)s"
+#
+#     def __init__(self, fmt="%(levelno)s: %(msg)s"):
+#         logging.Formatter.__init__(self, fmt)
+#
+#     def format(self, record):
+#
+#         # Save the original format configured by the user
+#         # when the logger formatter was instantiated
+#         format_orig = self._fmt
+#
+#         # Replace the original format with one customized by logging level
+#         if record.levelno == logging.DEBUG:
+#             self._fmt = MyLogFormatter.dbg_fmt
+#
+#         elif record.levelno == logging.INFO:
+#             self._fmt = MyLogFormatter.info_fmt
+#
+#         elif record.levelno == logging.ERROR:
+#             self._fmt = MyLogFormatter.err_fmt
+#
+#         elif record.levelno == logging.WARNING:
+#             self._fmt = MyLogFormatter.warn_fmt
+#
+#         # Call the original formatter class to do the grunt work
+#         result = logging.Formatter.format(self, record)
+#
+#         # Restore the original format configured by the user
+#         self._fmt = format_orig
+#
+#         return result
