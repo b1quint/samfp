6032f2fc4da2abb6794db64cff09deab6e38dd91
Bruno Quint <b1quint@gmail.com>
2018-03-28 10:56:45 -0300

    Unittest for logging working! o/

--- samfp/io/tests/test_logger.py
+++ samfp/io/tests/test_logger.py
@@ -2,28 +2,56 @@
 import io
 import logging
 import unittest
+import sys
 
-from samfp.io.logging import get_logger, SamFpLogFormatter
+from samfp.io.logging import get_logger
 
 
-class TestLogging(unittest.TestCase):
+class TestLogFormat(unittest.TestCase):
+
+    def setUp(self):
+        self.held_stdout, sys.stdout = sys.stdout, io.StringIO()
+        self.help_stderr, sys.stderr = sys.stderr, io.StringIO()
+
+    def tearDown(self):
+        sys.stdout = self.held_stdout
+        sys.stderr = self.help_stderr
 
     def test_debug(self):
+
         logger = get_logger('TestLogApp')
-        logger.debug('debug message')
+        logger.debug("debug message")
+        log_message = sys.stderr.getvalue().strip()
+
+        self.assertRegex(log_message, r'(\[D\s\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\s\w*\]\s)')
 
     def test_info(self):
         logger = get_logger('TestLogApp')
-        logger.info('info message')
+        logger.info("info message")
+        log_message = sys.stderr.getvalue().strip()
 
+        self.assertRegex(log_message, r'(\[I\s\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\s\w*\]\s)')
+
+    @unittest.skip
     def test_warning(self):
         logger = get_logger('TestLogApp')
-        logger.warning('warning message')
+        logger.warning("warning message")
+        log_message = sys.stderr.getvalue().strip()
 
+        self.assertRegex(log_message, r'(\[W\s\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\s\w*\]\s)')
+
+    @unittest.skip
     def test_error(self):
         logger = get_logger('TestLogApp')
-        logger.error('error message')
+        logger.error("error message")
+        log_message = sys.stderr.getvalue().strip()
 
+        self.assertRegex(log_message, r'(\[E\s\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\s\w*\]\s)')
+
+    @unittest.skip
     def test_critical(self):
         logger = get_logger('TestLogApp')
-        logger.critical('critical message')
+        logger.critical("critical message")
+        log_message = sys.stderr.getvalue().strip()
+
+        self.assertRegex(log_message, r'(\[C\s\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\s\w*\]\s)')
