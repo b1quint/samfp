d74a64d977e3f1bd0f082a8df00783aef8fb3754
Bruno Quint <b1quint@gmail.com>
2018-06-16 00:57:30 -0400

    Finished the first version of the pipeline.

--- scripts/sami_pipe.py
+++ scripts/sami_pipe.py
@@ -62,11 +62,14 @@ def main():
             zero_list_buffer.write('\n'.join(zero_files))
 
         for zero_file in zero_files:
-            zero_data = xjoin.get_joined_data(zero_file)
-            zero_header = pyfits.getheader(zero_file)
+            data = xjoin.get_joined_data(zero_file)
+            header = pyfits.getheader(zero_file)
+
+            data, header, prefix = xjoin.join_and_process(data, header)
+
             path, fname = os.path.split(zero_file)
             zero_file = os.path.join(path, 'RED', fname)
-            pyfits.writeto(zero_file, zero_data, zero_header)
+            pyfits.writeto(zero_file, data, header)
 
         ic = ccdproc.ImageFileCollection(
             location=os.path.join(PATH, 'RED'),
@@ -88,9 +91,81 @@ def main():
 
         zero_combine.run()
 
+        flat_table = sflat_table + dflat_table
+        filters_used = []
+        for row in flat_table:
+            if row['filter_id'] not in filters_used:
+                filters_used.append(row['filter_id'])
 
+        for _filter in filters_used:
 
+            sub_table_by_filter = [
+                row for row in flat_table if row['filter_id'] == _filter
+            ]
 
+            flat_list_name = os.path.join(
+                PATH, 'RED',
+                "1FLAT_{}x{}_{}".format(binning[0], binning[1], _filter)
+            )
+
+            flat_files = [row['filename'] for row in sub_table_by_filter]
+
+            with open(flat_list_name, 'w') as flat_list_buffer:
+                flat_list_buffer.write('\n'.join(flat_files))
+
+            for flat_file in flat_files:
+
+                xjoin.bias_file = master_zero_fname
+                d = xjoin.get_joined_data(flat_file)
+                h = pyfits.getheader(flat_file)
+
+                d, h, p = xjoin.join_and_process(d, h)
+
+                path, fname = os.path.split(flat_file)
+                flat_file = os.path.join(path, 'RED', fname)
+                pyfits.writeto(flat_file, d, h)
+
+            master_flat_fname = os.path.join(PATH, 'RED',
+                                             flat_list_name + '.fits')
+
+            flat_combine_files = [
+                os.path.join(PATH, 'RED', os.path.split(f)[-1])
+                for f in flat_files
+            ]
+
+            flat_combine = image_combine.FlatCombine(
+                 input_list=flat_combine_files,
+                 output_file=master_flat_fname
+            )
+
+            flat_combine.run()
+
+            sub_table_by_filter = [
+                row for row in obj_table if row['filter_id'] == _filter
+            ]
+
+            obj_list_name = os.path.join(
+                PATH, 'RED',
+                "2OBJECT_{}x{}_{}".format(binning[0], binning[1], _filter)
+            )
+
+            obj_files = [row['filename'] for row in sub_table_by_filter]
+
+            with open(obj_list_name, 'w') as obj_list_buffer:
+                obj_list_buffer.write('\n'.join(obj_files))
+
+            for obj_file in obj_files:
+
+                xjoin.bias_file = master_zero_fname
+                xjoin.flat_file = master_flat_fname
+                d = xjoin.get_joined_data(obj_file)
+                h = pyfits.getheader(obj_file)
+
+                d, h, p = xjoin.join_and_process(d, h)
+
+                path, fname = os.path.split(obj_file)
+                obj_file = os.path.join(path, 'RED', fname)
+                pyfits.writeto(obj_file, d, h)
 
 
 if __name__ == '__main__':
